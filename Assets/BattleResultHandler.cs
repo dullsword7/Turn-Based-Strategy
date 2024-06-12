f1041b9ba4db2abf130e24884d2538bcf66e4211
Alan Wu <57293716+dullsword7@users.noreply.github.com>
2024-06-11 22:34:41 -0400

    Added Calculation To Determine Number Of Attacks Based On Speed

--- Assets/BattleResultHandler.cs
+++ Assets/BattleResultHandler.cs
@@ -10,6 +10,7 @@ public class BattleResultHandler
 
     private TMP_Text attackingUnitText; 
     private TMP_Text battleResultText; 
+    private TMP_Text unitAttackCount; 
     private TMP_Text defendingUnitText;
 
     private BattleUnit currentAttackingUnit;
@@ -24,6 +25,7 @@ public class BattleResultHandler
 
         attackingUnitText =  attackingUnit.GetComponentInChildren<TMP_Text>();
         battleResultText =  battleResult.GetComponentInChildren<TMP_Text>();
+        this.unitAttackCount = unitAttackCount;
         defendingUnitText =  defendingUnit.GetComponentInChildren<TMP_Text>();
     }
 
@@ -44,33 +46,78 @@ public class BattleResultHandler
         battleResultHolder.SetActive(false);
     }
 
+    /// <summary>
+    /// Sets the text to information about the attacking unit.
+    /// </summary>
     public void SetAttackingUnitText()
     {
         attackingUnitText.SetText(currentAttackingUnit.BattleUnitInfo.BattleUnitName);
     }
 
+    /// <summary>
+    /// Sets the text to information about the battle result.
+    /// </summary>
     public void SetBattleResultText()
     {
         battleResultText.SetText($"{currentAttackingUnit.BattleUnitInfo.BattleUnitName} VS {currentDefendingUnit.BattleUnitInfo.BattleUnitName}");
     }
 
+    /// <summary>
+    /// Sets the text to the number of times the attacking unit will attack.
+    /// </summary>
+    public void SetUnitAttackCountText()
+    {
+        float attackCount = DetermineNumberOfAttacks();
+        if (attackCount > 1) unitAttackCount.SetText($"X {attackCount}");
+    }
+
+    /// <summary>
+    /// Setse the text to information about the defending unit.
+    /// </summary>
     public void SetDefendingUnitText()
     {
         defendingUnitText.SetText(currentDefendingUnit.BattleUnitInfo.BattleUnitName);
     }
 
+    /// <summary>
+    /// Used to assign the current attacking battle unit.
+    /// </summary>
+    /// <param name="currentAttackingUnit">the battle unit attacking</param>
     public void SetCurrentAttackingUnit(BattleUnit currentAttackingUnit)
     {
         this.currentAttackingUnit = currentAttackingUnit;
     }
+
+    /// <summary>
+    /// Used to assign the current defending battle unit.
+    /// </summary>
+    /// <param name="currentDefendingUnit">the battle unit defending</param>
     public void SetCurrentDefendingUnit(BattleUnit currentDefendingUnit)
     {
         this.currentDefendingUnit = currentDefendingUnit;
     }
+
+    /// <summary>
+    /// Updates the text of the battle result screen. 
+    /// </summary>
     public void UpdateBattleResultCanvas()
     {
         SetAttackingUnitText();
         SetBattleResultText();
+        SetUnitAttackCountText();
         SetDefendingUnitText();
+    }
+
+    /// <summary>
+    /// Calculates the number of times the attacking unit will attack.
+    /// </summary>
+    /// <returns>the number of times to attack</returns>
+    private int DetermineNumberOfAttacks()
+    {
+        float attackingUnitSpeed = currentAttackingUnit.BattleUnitStats[StatName.Speed];
+        float defendingUnitSpeed = currentDefendingUnit.BattleUnitStats[StatName.Speed];
+        if (attackingUnitSpeed > defendingUnitSpeed * 2) return 2;
+
+        return 1;
     }
 }
